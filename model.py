bd29a0e1db0a28a731a829997421fe22e6b83960
April <chitran17@augustana.edu>
2021-04-03 23:48:36 -0500

    seting up the model

--- model.py
+++ model.py
@@ -3,6 +3,22 @@ from math import pi
 
 
 class InputForm(Form):
-    A = FloatField(
-        label='amplitude (m)', default=1.0,
+    L = FloatField(
+        label='Length of the rod', default=1.0,
+        validators=[validators.InputRequired()])
+
+    Delta_x = FloatField(
+        label='Space step size', default=0.01,
+        validators=[validators.InputRequired()])
+
+    T = FloatField(
+        label='Time', default=pi,
+        validators=[validators.InputRequired()])
+
+    Delta_t = FloatField(
+        label='Space step size', default=0.01,
+        validators=[validators.InputRequired()])
+
+    Beta = FloatField(
+        label='Beta', default=1,
         validators=[validators.InputRequired()])
